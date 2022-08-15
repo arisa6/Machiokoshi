f83b4012ba9638cbc851879e4978de62ef71d8c0
EC2 Default User <ec2-user@ip-172-31-46-86.ap-northeast-1.compute.internal>
2022-08-15 16:59:31 +0000

    ひとつ前に合わせるため

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ApplicationRecord
     has_many :cart_items,dependent: :destroy
     has_many :order_details,dependent: :destroy
     
-    attachment :image
+    has_one_attached :image
     
     # validates :image,presence:true
     validates :name,presence:true
