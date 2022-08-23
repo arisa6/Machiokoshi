f8873eacebc98456381832928fcc344e7df21c21
EC2 Default User <ec2-user@ip-172-31-46-86.ap-northeast-1.compute.internal>
2022-08-23 19:12:12 +0000

    bootstrap icon CDNにて導入

--- app/controllers/public/genres_controller.rb
+++ app/controllers/public/genres_controller.rb
@@ -1,7 +1,18 @@
 class Public::GenresController < ApplicationController
   def show
     @genre = Genre.find(params[:id])
-    @items = @genre.item.where(is_active: true) #where条件に合致するオブジェクトを配列として取り出す
+    @items = @genre.items.where(is_active: true) #where条件に合致するオブジェクトを配列として取り出す
     @genres = Genre.all
+    @item = Item.find(params[:id])
+  end
+  
+  
+  private
+  def item_params
+  	params.require(:item).permit(:name, :image)
+  end
+  
+  def item_params
+  	params.require(:genre).permit(:name)
   end
 end
